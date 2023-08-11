299f574a2556fa2038c854646af6529855043d26
Mpendulo <mpendulo.dev@gmail.com>
2023-08-11 12:44:51 +0200

    delete book test

--- tests/app.test.js
+++ tests/app.test.js
@@ -39,4 +39,30 @@ describe("Book API", () => {
     expect(response.status).toEqual(201);
     expect(response.body).toMatchObject(newBook);
   });
+
+  //Update a book
+  it("should update a book", async () => {
+    const newBook = { title: "Test Book", genre: "Fiction" };
+    const createResponse = await request(app).post("/api/book").send(newBook);
+
+    const updatedBook = { title: "Updated book" };
+    const updateResponse = await request(app)
+      .put(`/api/book/${createResponse.body._id}`)
+      .send(updatedBook);
+
+    expect(updateResponse.status).toEqual(200);
+    expect(updateResponse.body.title).toEqual(updatedBook.title);
+  });
+
+  //Delete a book
+  it("should delete a book", async () => {
+    const newBook = { title: "Test Book" };
+    const createResponse = await request(app).post("/api/book").send(newBook);
+
+    const deleteResponse = await request(app).delete(
+      `/api/book/${createResponse.body._id}`
+    );
+
+    expect(deleteResponse.status).toEqual(204);
+  });
 });
